d1a865a6f72c69e3192aed0b69de059953815033
Medion M Narcisse <medionnarcisse68@gmail.com>
2025-08-01 22:07:35 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+  key_name = "devops-nabster"
+  tags = {
+    Name = "ec2-ciscko"
+  }
+}
